d049e1ba9848885dc189a19eb0388220649c8a9a
tingtt <ting.taku@gmail.com>
2023-02-02 08:39:43 +0900

    feat(hello): change message

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ const html = `<html>
 </head>
 <body class="bg-black">
 	<div class="h-screen flex items-center justify-center">
-		<div class="text-6xl text-white">Hello, systemd-cd!</div>
+		<div class="text-6xl text-white">Hello, world!</div>
 	</div>
 </body>
 </html>`
