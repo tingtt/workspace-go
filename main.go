07a92c9b54a2eb8f6045aba4b5d6ac89804b8bb0
tingtt <ting.taku@gmail.com>
2023-02-02 11:40:11 +0900

    feat(hello): change message and coolor

--- main.go
+++ main.go
@@ -16,7 +16,13 @@ const html = `<html>
 </head>
 <body class="bg-black">
 	<div class="h-screen flex items-center justify-center">
-		<div class="text-6xl text-white">Hello, world!</div>
+		<div class="text-6xl text-white">
+			Hello,
+			<span
+				class="w-[fit-content] text-[4rem] text-black text-opacity-0 bg-[#2B90B6] bg-cover bg-clip-text bg-gradient-to-r from-fuchsia-400 to-fuchsia-700 h-[70px]"
+				>systemd-cd!</span
+			>
+		</div>
 	</div>
 </body>
 </html>`
